6aa4b01faa02f4a6d518fd70724c12e38d08c09c
Jose Manuel <sanchezmosquerajosemanuel@gmail.com>
2022-05-26 15:04:53 +0200

    Add method for print the Hex game board

--- TestHexBoard.cpp
+++ TestHexBoard.cpp
@@ -8,34 +8,60 @@
 
 class hexGraph{
 	public:
-		hexGraph(){
+		hexGraph(int size): size(size){
 			int count = 0;
-			edgelist.resize(121);
-			//for(int i=0; i<11; i++)
-				//for(int j=0; j<11; j++)
+			edgeList.resize(size);
+			for(int i=0; i<size; i++)
+				for(int j=0; j<size; j++)
+					edgeList[i, j].push_back(0);
 					//makeNode(i, j, edgelist[count++]);	
 		}
+		
+		void plotHexBoard(){
+			
+			// Plot board header
+			std::cout << "******** HEX GAME *********" << std::endl;
+			std::cout << std::endl;
+			
+			std::string space = "";
+			std::string edges = "\\";
+			for (int i=1; i<size; i++){
+				edges += " / \\";
+			}
+			
+			// Plot first line
+			std::cout << edgeList[0][0];
+			for (int j=1; j<size; j++)
+				std::cout << " - " << edgeList[0][j];
+			std::cout << std::endl;
+				
+			// Plot hex board
+			for (int i=1; i<size; i++){
+				space += ' ';
+				std::cout << space << edges << std::endl;
+				space += ' ';
+				std::cout << space << edgeList[i][0];
+				for (int j=1; j<size; j++)
+					std::cout << " - " << edgeList[i][j];
+			std::cout << std::endl;
+			}
+			
+			// Plot board legend
+			std::cout << std::endl;
+			std::cout << "Up-Down: RED, Left-Right: BLUE" << std::endl;
+			std::cout << "***************************" << std::endl;		
+			std::cout << std::endl;		
+		}
 	private:
-		std::vector<std::deque<int> > edgelist;
+		int size = 0;
+		std::vector<std::deque<int> > edgeList;
 };
 
 
 int main(){
+
+	int boardSize = 5;
 	
-	int size = 5;
-	std::vector<std::vector<int> > edgeList;
-	edgeList.resize(size*size);
-	for(int i=0; i<size; i++)
-		for(int j=0; j<size; j++)
-			edgeList[i, j].push_back(0);
-	
-	// Print edgeList
-	for ( const auto &row : edgeList ){
-		for ( const auto &s : row ) std::cout << s << ' ';
-		std::cout << std::endl;
-	}
-	
-	// Print part of edgeList
-	for(auto i: edgeList[0])
-		std::cout << i << ' ';
+	hexGraph TestGraph(boardSize);	
+	TestGraph.plotHexBoard();
 }
